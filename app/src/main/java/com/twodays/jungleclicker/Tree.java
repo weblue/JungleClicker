724603b796083f87170656f77a36443e27af123f
weblue <naderki@vt.edu>
2016-12-01 01:03:41 -0500

    fixed save load

--- app/src/main/java/com/twodays/jungleclicker/Tree.java
+++ app/src/main/java/com/twodays/jungleclicker/Tree.java
@@ -47,24 +47,39 @@ public class Tree {
         treeAsyncTask.execute();
     }
 
+
     public void save() {
         SharedPreferences sharedPref = activity.getCurrentActivity().getPreferences(Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putInt("pref_title_total_coconuts", totalCoconuts);
-        editor.putInt("pref_title_times_clicked", timesClicked);
-        editor.putInt("pref_title_coconuts_spent", coconutsSpent);
-//        for(int i = 0)
-//        editor.putInt("pref_title_click_upgrades", clickUpgrades);
-//        editor.putInt("pref_title_generate_spent", genUpgrades);
-        editor.commit();
+        editor.putInt("pref_key_total_coconuts", totalCoconuts);
+        editor.putInt("pref_key_coconuts", coconuts);
+        editor.putInt("pref_key_times_clicked", timesClicked);
+        editor.putInt("pref_key_coconuts_spent", coconutsSpent);
+        for(int i = 0;i<5;i++){
+            editor.putInt("pref_key_click_upgrades"+i, clickUpgrades[i]);
+            editor.putInt("pref_key_generate_spent"+i, genUpgrades[i]);
+        }
+        editor.apply();
     }
 
     public void load() {
         SharedPreferences sharedPref = activity.getCurrentActivity().getPreferences(Context.MODE_PRIVATE);
         int defaultValue = 0;
-        totalCoconuts = sharedPref.getInt("pref_title_total_coconuts", defaultValue);
-        coconutsSpent = sharedPref.getInt("pref_title_coconuts_spent", defaultValue);
-        timesClicked = sharedPref.getInt("pref_title_times_clicked", defaultValue);
+        totalCoconuts = sharedPref.getInt("pref_key_total_coconuts", defaultValue);
+        Log.d("tree", totalCoconuts + "total nuts");
+        coconuts = sharedPref.getInt("pref_key_coconuts", defaultValue);
+        coconutsSpent = sharedPref.getInt("pref_key_coconuts_spent", defaultValue);
+        timesClicked = sharedPref.getInt("pref_key_times_clicked", defaultValue);
+        for(int i = 0;i<5;i++){
+            if (i==0){
+                clickUpgrades[i]=sharedPref.getInt("pref_key_click_upgrades"+i, 1);
+                genUpgrades[i]=sharedPref.getInt("pref_key_generate_spent"+i, 1);
+            }
+            else {
+                clickUpgrades[i] = sharedPref.getInt("pref_key_click_upgrades" + i, defaultValue);
+                genUpgrades[i] = sharedPref.getInt("pref_key_generate_spent" + i, defaultValue);
+            }
+        }
     }
 
     public void click() {

df571bb4cb0f93700df9f52ae304b0437a017bb9
weblue <naderki@vt.edu>
2016-12-01 00:31:38 -0500

    stats are done

--- app/src/main/java/com/twodays/jungleclicker/SettingsActivity.java
+++ app/src/main/java/com/twodays/jungleclicker/SettingsActivity.java
@@ -20,22 +20,22 @@ public class SettingsActivity extends PreferenceActivity {
 
         findPreference("pref_key_times_clicked").setSummary(MainActivity.tree.getTimesClicked() + " times clicked.");
 
-        findPreference("pref_key_click_upgrades0").setSummary("Snake purchased " + MainActivity.tree.getClickUpgrades()[0] + " times.");
+        findPreference("pref_key_click_upgrades1").setSummary("Snake purchased " + MainActivity.tree.getClickUpgrades()[0] + " times.");
 
-        findPreference("pref_key_click_upgrades1").setSummary("Giraffe purchased " + MainActivity.tree.getClickUpgrades()[1] + " times.");
+        findPreference("pref_key_click_upgrades2").setSummary("Giraffe purchased " + MainActivity.tree.getClickUpgrades()[1] + " times.");
 
-        findPreference("pref_key_click_upgrades2").setSummary("Tiger purchased " + MainActivity.tree.getClickUpgrades()[2] + " times.");
+        findPreference("pref_key_click_upgrades3").setSummary("Tiger purchased " + MainActivity.tree.getClickUpgrades()[2] + " times.");
 
-        findPreference("pref_key_click_upgrades3").setSummary("Parrot purchased " + MainActivity.tree.getClickUpgrades()[3] + " times.");
+        findPreference("pref_key_click_upgrades4").setSummary("Parrot purchased " + MainActivity.tree.getClickUpgrades()[3] + " times.");
 
-        findPreference("pref_key_gen_upgrades0").setSummary("Gloves purchased " + MainActivity.tree.getGenUpgrades()[0] + " times.");
+        findPreference("pref_key_generate_spent0").setSummary("Gloves purchased " + MainActivity.tree.getGenUpgrades()[0] + " times.");
 
-        findPreference("pref_key_gen_upgrades1").setSummary("Shovel purchased " + MainActivity.tree.getGenUpgrades()[1] + " times.");
+        findPreference("pref_key_generate_spent1").setSummary("Shovel purchased " + MainActivity.tree.getGenUpgrades()[1] + " times.");
 
-        findPreference("pref_key_gen_upgrades2").setSummary("Chainsaw purchased " + MainActivity.tree.getGenUpgrades()[2] + " times.");
+        findPreference("pref_key_generate_spent2").setSummary("Chainsaw purchased " + MainActivity.tree.getGenUpgrades()[2] + " times.");
 
-        findPreference("pref_key_gen_upgrades3").setSummary("Bulldozer purchased " + MainActivity.tree.getGenUpgrades()[3] + " times.");
+        findPreference("pref_key_generate_spent3").setSummary("Bulldozer purchased " + MainActivity.tree.getGenUpgrades()[3] + " times.");
 
-        findPreference("pref_key_gen_upgrades4").setSummary("Excavator purchased " + MainActivity.tree.getGenUpgrades()[4] + " times.");
+        findPreference("pref_key_generate_spent4").setSummary("Excavator purchased " + MainActivity.tree.getGenUpgrades()[4] + " times.");
     }
 }
